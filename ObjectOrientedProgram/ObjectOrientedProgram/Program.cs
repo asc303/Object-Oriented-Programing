0afdb7f35a587efc19baaec81d7054d33852e793
asc303 <chauhansam94@gmail.com>
2021-11-18 23:54:36 +0530

    [ashutosh] add:Stock Management

---
+++ ObjectOrientedProgram/ObjectOrientedProgram/Program.cs
@@ -0,0 +1,41 @@
+﻿using ObjectOrientedProgram;
+using System;
+
+namespace ObjectOrientedProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Welcome to Object Oriented Program");
+                Console.WriteLine("Choose the Option : \n 1. JSON \n 2. Inventory Management \n 3. Stock List \n 4. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        InventoryMain inv = new InventoryMain();
+                        inv.DisplayData(@"D:\git project\ObjectOrientedProgram\ObjectOrientedProgram\ObjectOrientedProgram\Files\Inventory.json");
+                        break;
+                    case 2:
+                        InventoryManagementMain inven = new InventoryManagementMain();
+                        inven.ReadData(@"C:\Users\gunas\OneDrive\Documents\Git Problems\ObjectOrientedProgram\ObjectOrientedProgram\ObjectOrientedProgram\Files\InventoryList.json");
+                        inven.DisplayData("Rice");
+                        inven.DisplayData("Wheat");
+                        inven.DisplayData("Pulses");
+                        break;
+                    case 3:
+                        StockMain stock = new StockMain();
+                        Console.WriteLine("The Stock Report is : ");
+                        stock.DisplayData(@"D:\git project\ObjectOrientedProgram\ObjectOrientedProgram\ObjectOrientedProgram\Files\Stock.json");
+                        break;
+                    case 4:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
